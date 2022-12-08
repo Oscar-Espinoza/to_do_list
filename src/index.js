1a0369979b852d3d83a921dd4127ae962cb8ef5f
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-12-08 15:34:39 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import './styles/main.scss'
+
+const tasks = [
+  {
+    descrition: 'Wash dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    descrition: 'Complete To do list project',
+    completed: false,
+    index: 1,
+  }
+]
+
+const addTask = (task) => {
+  const taskList = document.getElementById('tasks-list');
+  const newTask = document.createElement('li');
+  newTask.classList.add('task');
+  newTask.id = `task-${task.id}`
+  newTask.innerHTML = `
+  <input type='checkbox' name='isTaskCompleted' id='check-${task.id}' value='${task.completed}'>
+  <p class='description'>${task.descrition}</p>
+  <img src='../src/images/dots.png' alt='three dots icon' class='three-dots'>
+  `;
+  taskList.insertBefore(newTask, taskList.lastChild)
+}
+
+tasks.forEach(task => {
+  addTask(task)
+});
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To do list',
+      template: './src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
