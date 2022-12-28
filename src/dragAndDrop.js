c7a960f3fc57d503aa651fc918ed65ec44ba0d33
Oscar-Espinoza <oscardavid2094@gmail.com>
2022-12-28 18:11:16 -0300

    remove task is working

--- src/dragAndDrop.js
+++ src/dragAndDrop.js
@@ -1,15 +1,32 @@
 const arrayUpdate = (draggedIndex, dropIndex) => {
-  const tasksList = JSON.parse(localStorage.getItem('tasks'))
+  if (draggedIndex !== dropIndex) {
+    const tasksList = JSON.parse(localStorage.getItem('tasks'))
+    const temp = tasksList[draggedIndex]
+  
+    for (let i = draggedIndex; i <= dropIndex; i++) {
+      
+      if (i !== dropIndex) {
+        tasksList[i] = tasksList[i + 1];
+      } else {
+        tasksList[i] = temp;
+      }    
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasksList))
+  }
 };
 
 export const drop = (task, mouseYPosition) => {
   const taskList = task.parentNode;
   const taskProperties = task.getBoundingClientRect();
   const draggedTask = document.querySelector('.dragging');
+  const draggedIndex = parseInt(draggedTask.id.slice(-1)) - 1
+  const dropIndex = parseInt(task.id.slice(-1)) - 1
 
   if (mouseYPosition <= (taskProperties.y + (taskProperties.height/2))) {
     taskList.insertBefore(draggedTask, task);
+    arrayUpdate(draggedIndex, dropIndex - 1)
   } else {
     taskList.insertBefore(draggedTask, task.nextSibling);
+    arrayUpdate(draggedIndex, dropIndex)
   }
 };
